1e5af8a137fb4328ba59c002bff7eae14999c822
Chris <christianguerrero98@gmail.com>
2021-01-05 20:10:18 -0600

    Created a function that runs an array of numbers through luhn's algorithm. Needs work with adding the index values together rather than concatenating, as well as subtracting 9 from each index that reaches more than 9 after being doubled. Function will be used in the main validate function inside the IF statement to finally determine if the number is indeed valid

--- js/myScript.js
+++ js/myScript.js
@@ -13,19 +13,22 @@ function Validate (){
   // document.querySelector('#status').innerHTML = masterCard.test(cardNumber);
   // document.querySelector('#status').innerHTML = amex.test(cardNumber);
 
+
+// Check what kind of card is being used.
   if (visa.test(cardNumber)){
-    document.querySelector('#status').innerHTML = 'VISA';
+    document.querySelector('#status').innerHTML = 'VISA: ';
   }else if (masterCard.test(cardNumber)) {
-    document.querySelector('#status').innerHTML = 'Master Card';
+    document.querySelector('#status').innerHTML = 'Master Card: ';
   }else if (amex.test(cardNumber)) {
-    document.querySelector('#status').innerHTML = 'American Express';
+    document.querySelector('#status').innerHTML = 'American Express: ';
   }else {
     document.querySelector('#status').innerHTML = 'This is not a valid card';
   }
 
+  let numbers = cardNumber.split('');
+  // console.log(numbers)
 
-
-
+  luhnsAlgorithm(numbers);
 
 // VISA
 // 4012888888881881
@@ -35,5 +38,21 @@ function Validate (){
 
 // AMEX
 // 371449635398431
+
+}
+
+function luhnsAlgorithm(numbers){
+// Function that passes an array through it and returns True or false to see whether the number is a muliple of ten
+let sum = 0;
+  for (var i=0; i<numbers.length-1; i++){
+    if (i%2 == 0 ){
+      numbers[i] = numbers[i] * 2
+      console.log(typeof numbers[i]);
+    }else {
+      sum = sum + numbers[i]
+    }
+  }
+  // Each index is being concatenated instead of being added to a total sum. Also need to take care of any numbers that are higher than 9 :\
+  console.log(sum)
 
 }
